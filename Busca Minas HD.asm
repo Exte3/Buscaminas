f5ef53a7b359552d61b70dc38de0a8b494d730d9
Gabriel Fuentes <nunkmeconecto@gmail.com>
2016-01-04 17:34:24 -0300

    El buscamina logro poner las minas en el tablero :dancer:
    Party Hard :dancers:  :dancer:
    
    La función desarrollada en localtest, se implemento en el "Buscaminas
    Remake HD"

--- Busca Minas HD.asm
+++ Busca Minas HD.asm
@@ -63,10 +63,13 @@ inicio:
 		move $a0, $v0
 		mul $a0, $a0, $a0
 		move $a1, $a0
-		add $a0, $zero, $zero
+		# int i = 0
 		add $t0, $zero, $zero
-		lw $t1, -4($a0)
-		addi $t1, $t1, 4
+		addiu $sp, $sp, -4
+		jal crear
+		mul $t0, $t0, 4
+		addu $sp, $sp, $t0
+		
 		jal crear
 		
 		j end
@@ -88,13 +91,20 @@ inicio:
 		crear:
 			# -4(pos) corresponde al primer dato del arreglo.
 			# La cabezera debe estar vacia
-			bge $t0, $t1, return
+			bge $t0, $a1, return
+			move $t7, $ra
+			move $t8, $a1
+			jal random
+			ble $a0, 15, mina
+			bgt $a0, 15, nada 
+			seguir:		
+			move $ra, $t7
+			move $a1, $t8
 			#cargar dato en $t1 y luego moverse
-			subi $t1, $t1, 4
-			lw $t3, -4($t1)
-			# Guarda un dato en la posicion correspondiete
 			
-			sw $t3, -4($t1)
+			
+			subi $t1, $t1, 4
+			# Guarda un dato en la posicion correspondiete
 			# i++
 			addi $t0, $t0, 1
 			j crear
@@ -109,7 +119,31 @@ inicio:
 			li $a1, 100
 			li $v0, 42
 			syscall
+			jr $ra
 			#Si el numero que retorna es igual o menor a 15 sera una mina
+			
+		mina:
+			# 9 signifuca mina
+			li $t3, 9
+			move $t5, $t0
+			sw $t3, -4($t1)
+			lw $t0, -4($t1)
+			move $t0, $t5
+			add $t5, $zero, $zero
+			j seguir
+		nada:	
+			#move $t5, $t0
+			#li $t5, 0	
+			sw $t0, -4($t1)
+			lw $t0, -4($t1)
+			#move $t0, $t5
+			j seguir
+	imprimir:
+
+		lw $t3, -16($sp)
+		move $a0, $t3
+		li $v0, 1
+		syscall
 			
 
 end:
--- Localtest.asm
+++ Localtest.asm
@@ -1,12 +1,14 @@
 		# $a0 es ingresado por teclado
-		li $a0, 4
+		li $a0, 16
 		
 		mul $a0, $a0, $a0
 		move $a1, $a0
 		# int i = 0
 		add $t0, $zero, $zero
-		lw $t1, 0($sp)
+		addiu $sp, $sp, -4
 		jal crear
+		mul $t0, $t0, 4
+		addu $sp, $sp, $t0
 		jal imprimir
 		j end
 crear:
@@ -17,12 +19,14 @@ crear:
 			move $t8, $a1
 			jal random
 			ble $a0, 15, mina
-			
+			bgt $a0, 15, nada 
 			seguir:		
 			move $ra, $t7
 			move $a1, $t8
 			#cargar dato en $t1 y luego moverse
-			lw $t1, -4($t1)
+			
+			
+			subi $t1, $t1, 4
 			# Guarda un dato en la posicion correspondiete
 			# i++
 			addi $t0, $t0, 1
@@ -37,14 +41,24 @@ crear:
 			syscall
 			jr $ra
 		mina:
-				# 9 signifuca mina
-				li $t3, 9
-				sw $t3, -4($t1)
-				j seguir
-
+			# 9 signifuca mina
+			li $t3, 9
+			move $t5, $t0
+			sw $t3, -4($t1)
+			lw $t0, -4($t1)
+			move $t0, $t5
+			add $t5, $zero, $zero
+			j seguir
+		nada:	
+			#move $t5, $t0
+			#li $t5, 0	
+			sw $t0, -4($t1)
+			lw $t0, -4($t1)
+			#move $t0, $t5
+			j seguir
 imprimir:
 
-	lw $t3, -8($sp)
+	lw $t3, -16($sp)
 	move $a0, $t3
 	li $v0, 1
 	syscall
